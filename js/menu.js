ba2d48d35079741f0c655b2332156a3ef946f740
olynka <98587532+olynka@users.noreply.github.com>
2022-09-12 11:59:06 +0300

    fix-footer

--- js/menu.js
+++ js/menu.js
@@ -15,3 +15,74 @@
     refs.body.classList.toggle("no-scroll");
   }
 })();
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+import NewApiServise from './api-servise';
+import TrendingMovesfilm from './trendingMovies'
+import CardTemplates from './cardTemplates';
+//об'єкт налаштувань
+const options = {
+    totalItems: 0,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    page: 1,
+}
+//повертає поточну сторінку
+
+const formEl = document.querySelector('input__wraper')
+console.log(formEl);
+const container = document.querySelector('#pagination');
+const pagination = new Pagination(container, options);
+
+const page = pagination.getCurrentPage();
+console.log(page);//повертає поточну сторінку
+
+
+const ApiServise = new NewApiServise();
+
+const cardTemplates=new CardTemplates()
+
+ApiServise.getTrendingMovie(page).then(({ total_results }) => { pagination.reset(total_results) }).catch(error=>(error.masage))
+
+ ApiServise
+    .getTrendingMovies()
+    .then(data => {
+      const movieCard = createMovieCard(data.results);
+     console.log(movieCard);
+    })
+    .then(data => {
+      refs.mainList.insertAdjacentHTML('beforeend', data);
+    });
+
+
+
+
+
+//  pagіnation.on('afterMove',upDatePagіnation)
+
+// function upDatePagіnation(e) {
+//      const currentPage = e.page;
+//     console.log(currentPage);
+ 
+//   ApiServise
+//     .getTrendingMovies(currentPage)
+//       .then(({data,total_results}) => {
+//         pagination.reset(total_results)
+//       const movieCard = createMovieCard(data.results);
+//       return movieCard;
+//     })
+//     .then(data => {
+//       refs.mainList.insertAdjacentHTML('beforeend', data);
+//     });
+
+//      ApiServise
+//     .getTrendingMovies(currentPage)
+//       .then(({data,total_results}) => {
+//         pagination.reset(total_results)
+//       const movieCard = createMovieCard(data.results);
+//       return movieCard;
+//     })
+//     .then(data => {
+//       refs.mainList.insertAdjacentHTML('beforeend', data);
+//     });
+// }
